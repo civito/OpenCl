c89f131f031d3f89a22c0f2e5e928f9fc62ddad9
Civito <hyunddol29@gmail.com>
2018-03-18 13:34:00 +0900

    make kernel

--- Project/test2.c
+++ Project/test2.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 #include<CL/cl.h>
+#include<stdlib.h>
+
+#define CHECK_ERROR(err) \
+	if(err != CL_SUCCESS) { \
+		printf("[%s:%d] OpenCl error %d\n", __FILE__, __LINE__, err); \
+	exit(EXIT_FAILURE); \
+	}
+
+#define MAX_SOURCE_SIZE (0x1000000)
 
 int main(){
 	char info[1024];
@@ -54,7 +63,104 @@ int main(){
 	clGetDeviceInfo(device, CL_DEVICE_HOST_UNIFIED_MEMORY, sizeof(cl_bool), &info_bool, NULL);
 	printf("IS Host and Device share Momory? : %d\n", info_bool);
 
+	/* Create Context */
+	cl_context context;
+	cl_int err;
+
+	context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
+	CHECK_ERROR(err);
+
+	/* Create Command Queue*/
+	cl_command_queue queue;
+	queue = clCreateCommandQueue(context, device, 0, &err);
+	CHECK_ERROR(err);
+
+	/* Build Program */
+	FILE *fp;
+	const char fileName[] = "./kernel.cl";
+	size_t source_size;
+	char *source_str;
+
+	fp = fopen(fileName, "r");
+	if(!fp){
+		fprintf(stderr, "Failed to load kernel.\n");
+		exit(1);
+	}
+
+	source_str = (char *)malloc(MAX_SOURCE_SIZE);
+	source_size = fread(source_str, 1 , MAX_SOURCE_SIZE, fp);
+	fclose(fp);
+
+	cl_program program;
+	program = clCreateProgramWithSource(context, 1, (const char**)&source_str, (const size_t*)&source_size, &err);
+	CHECK_ERROR(err);
 	
+	err = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
+	CHECK_ERROR(err);
+
+	/* Kernel Object */
+	cl_kernel kernel_vec_add;
+	kernel_vec_add = clCreateKernel(program, "vec_add", &err);
+
+	/* Create Buffer */
+	float *A, *B, *C;
+	int i;
+
+	A = (float*)malloc(sizeof(float)*4*4);
+	B = (float*)malloc(sizeof(float)*4*4);
+	C = (float*)malloc(sizeof(float)*4*4);
+
+	for(i = 0; i < 16; i++ ){
+		A[i] = i + 1;
+		B[i] = i * 2;
+	}
+	
+	cl_mem bufA, bufB, bufC;
+	bufA = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(float)*4*4, NULL, &err);
+	bufB = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(float)*4*4, NULL, &err);
+	CHECK_ERROR(err);
+
+
+	err = clEnqueueWriteBuffer(queue, bufA, CL_TRUE, 0, sizeof(float)*4*4,A,0,NULL,NULL);
+	CHECK_ERROR(err);
+	err = clEnqueueWriteBuffer(queue, bufB, CL_TRUE, 0, sizeof(float)*4*4,B,0,NULL,NULL);
+	CHECK_ERROR(err);
+
+	err = clEnqueueReadBuffer(queue, bufB, CL_TRUE,0,sizeof(float)*4*4,C,0,NULL,NULL);
+	CHECK_ERROR(err);
+	
+	
+	printf("Read Buffer : \n");
+	for(i=0; i<16; i++){
+		printf(" %f \n", C[i]);
+	}
+
+
+	
+
+
+
+
+
+	/* Release Source */
+	clReleaseKernel(kernel_vec_add);
+	clReleaseProgram(program);
+	clReleaseCommandQueue(queue);
+	clReleaseContext(context);
+
+
+	
+
+	
+
+
+	
+
+
+
+
+
+
 
 	
 
